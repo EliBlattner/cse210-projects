6bea7ef264130ba336e8d71acd72dfedcc6a8a2b
Eli Blattner <139493532+EliBlattner@users.noreply.github.com>
2023-09-23 17:49:03 -0600

    .

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
@@ -7,7 +8,26 @@ class Program
         Console.WriteLine("Grade Calculator Version 1");
         Console.WriteLine("Please enter the grade percentage you recieved:");
         string gradePercentage = Console.ReadLine();
-        Console.WriteLine($"{int.Parse(gradePercentage)} is your grade percentage.");
-        
+        int gradePercentageNum = int.Parse(gradePercentage);
+
+        if (gradePercentageNum > 70)
+        {
+            Console.WriteLine($"{int.Parse(gradePercentage)} is your grade percentage. Great job!");
+        }
+        else if (gradePercentageNum > 50)
+        {
+            Console.WriteLine("Darn, you didn't pass the class. Better luck next time!");
+        }
+        else if (gradePercentageNum > 30)
+        {
+            Console.WriteLine("Yikes, that's not a very good grade...you need some help!");        }
+        else if (gradePercentageNum > 15)
+        {
+            Console.WriteLine("Go talk to academic advising or something!");
+        }
+        else
+        {
+            Console.WriteLine("Drop the class before it's too late!");
+        }
     }
 }
