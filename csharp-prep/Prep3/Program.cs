19363848fdf23829b2d90204a1b4f42a1a13c73c
Eli Blattner <139493532+EliBlattner@users.noreply.github.com>
2023-09-23 18:44:44 -0600

    .

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        //Console.WriteLine("What is your magic number?");
+        //string magicNumber = Console.ReadLine();
+        //int magicNumberNum = int.Parse(magicNumber);
+
+        bool guess = false;
+
+        while (guess == false)
+        {
+            Console.WriteLine("What is your guess?");
+            string userGuess = Console.ReadLine();
+            int userGuessNum = int.Parse(userGuess);
+            if (number > userGuessNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < userGuessNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Nice job! You guessed it!");
+                guess = true;
+            }
+        }
     }
 }
