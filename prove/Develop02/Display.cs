a728105816653ef0a39d7162d058a20b69dc15b7
EliBlattner <139493532+EliBlattner@users.noreply.github.com>
2023-10-06 21:15:36 -0600

    .

---
+++ prove/Develop02/Display.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Display
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please select an operation 1-5: \n1. Write\n2. Display\n3. Load\n4. Save\n5. Quit\n----------------");
+        string userInput = Console.ReadLine();
+        
+
+    }
+}
