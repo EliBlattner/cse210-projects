7dc73b052ec6f52f21f717a9f5e876e490348e26
EliBlattner <139493532+EliBlattner@users.noreply.github.com>
2023-10-09 02:15:48 -0600

    .

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,26 +5,47 @@ public class Program
 {
     static void Main(string[] args)
     {
-        string userLoopInput = "999";
+        string userLoopInput = "0";
 
         //Write class operations below
         Write write1 = new Write();
-        write1._userEntry = Console.ReadLine();
-        write1._currentDateTime = DateTime.Now;
 
-        Show show1 = new Show();
-        show1._entriesList.Add(write1.Display());
-
-        while (userLoopInput != "0"); 
+        while (userLoopInput != "5")
         {
             Console.WriteLine("Please enter an Input from 1-5.\n1. Write\n2. Show\n3. Save\n4. Load\n5. Quit");
-            
-            if (userLoopInput == "1");
-            {
+            Console.WriteLine("What is your input?: ");
+            userLoopInput = Console.ReadLine();
 
+
+            if (userLoopInput == "1")
+            {   
+                write1.Display2();
+                write1._userEntry = Console.ReadLine();
+                write1._currentDateTime = DateTime.Now;
+                //ADD this to a list to keep it
+                
+                write1._entriesList.Add(write1.Display());
             }
 
-            else i
+            else if (userLoopInput == "2")
+            {
+                Console.WriteLine("\n---------------\nJournal Entries\n--------------\n");
+                
+                 for (int i = 0; i < write1._entriesList.Count; i++)
+                    {
+                        Console.WriteLine($"{write1._entriesList[i]}\n");
+                    }
+            }
+
+            else if (userLoopInput == "3")
+            {
+                Console.WriteLine("Save Call");
+            }
+
+            else
+            {
+                Console.WriteLine("Load Call");
+            }
         }
 
 
--- prove/Develop02/Write.cs
+++ prove/Develop02/Write.cs
@@ -3,6 +3,7 @@ using System.Security.Cryptography.X509Certificates;
 
 public class Write
 {
+
     public string _userEntry;
     public DateTime _currentDateTime;
 
@@ -12,12 +13,28 @@ public class Write
 
     int randomIndex = random.Next(0,3);
     
-    
+    public List<string> _entriesList = new List<string>();
+
+
     public string Display()
     {
         string _currentDateTimeStr = _currentDateTime.ToString("yyyy-MM-dd HH:mm:ss");
         Console.WriteLine($"{_currentDateTimeStr} , {_promptBankList[randomIndex]} , {_userEntry}");
         return $"{_currentDateTimeStr} , {_promptBankList[randomIndex]} , {_userEntry}";
     } 
-   
+   public string Display2()
+   {
+        Console.WriteLine(_promptBankList[randomIndex]);
+        return _promptBankList[randomIndex];
+
+   }
+
+   public string Display3()
+   {
+        string _currentDateTimeStr = _currentDateTime.ToString("yyyy-MM-dd HH:mm:ss");
+        string result = $"{_currentDateTimeStr} , {_promptBankList[randomIndex]} , {_userEntry}"; // Call the Display method and capture its result
+        _entriesList.Add(result); // Add the result to the _entriesList
+        return result;
+        
+   }
 }
