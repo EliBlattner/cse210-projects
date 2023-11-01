794a7a2529c952b5f72c0832d82e21eaf8679dc3
EliBlattner <139493532+EliBlattner@users.noreply.github.com>
2023-10-31 21:24:55 -0600

    this took so long smh

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -5,20 +5,76 @@ public class Reference
 
     private bool _multiVersesExist;
 
+    private string _bookName;
+
+    private string _verseNumber1;
+
+    private string _verseNumber2;
+
+    private string _verseText1;
+
+    private string _verseText2;
+
+    private string _chapterNumber;
+
 
     //Uses logic to assign _multiVerseExists member variable true or false if reference contains either 1 or 2 verses.
     //This will be used to tell the constructors in the Scripture class how to behave. 
-    private Reference()
+    public Reference()
     {
         Console.WriteLine("Hello! Does your scripture reference contain more than one verse? (yes/no): ");
-        string multipleVerses = Console.ReadLine();
-        if (multipleVerses == "yes")
+        string multipleVersesInput = Console.ReadLine();
+        Console.WriteLine("What is the name of the book the scripture comes from?: ");
+        _bookName = Console.ReadLine();
+        Console.WriteLine("What is the chapter number the verse is found in?: ");
+        _chapterNumber = Console.ReadLine();
+
+
+        if (multipleVersesInput == "yes")
         {
             _multiVersesExist = true;
+            
+            Console.WriteLine("Which number verse is the first verse?: ");
+            _verseNumber1 = Console.ReadLine();
+            Console.WriteLine("Which number verse is the second verse?: ");
+            _verseNumber2 = Console.ReadLine();
+            Console.WriteLine("What is the content of the first verse? (copy and paste the text): ");
+            _verseText1 = Console.ReadLine();
+            Console.WriteLine("What is the content of the second verse? (copy and past the text): ");
+            _verseText2 = Console.ReadLine();
+
+
         }
         else
         {
             _multiVersesExist = false;
+
+            Console.WriteLine("Which number is the verse?: ");
+            _verseNumber1 = Console.ReadLine();
+            Console.WriteLine("What is the content of the verse? (copy and past the text): ");
+            _verseText1 = Console.ReadLine();
+
         }
     }
-}
+
+    //This method takes all the attributes and passes them in an encapsulated form as a collection of return values.
+    public (bool, string, string, string, string, string, string) PassReferenceValues()
+    {
+        return (_multiVersesExist,_verseNumber1,_verseNumber2,_verseText1,_verseText2,_bookName,_chapterNumber);
+    }
+
+    //This method's sole purpose is to enable the member variables defined in this constructor to be passed over to
+    //the ParseScripCreateList method within the Scripture class.
+    public void ProcessReferenceValuesInScripture(Scripture scripture)
+    {
+        var values = PassReferenceValues();
+        scripture.ParseScripCreateList(values.Item1, values.Item4, values.Item5);
+    }
+
+    //This does the same thing but for the methods in the Word class.
+    
+    //public void WordClassMethodNULL(Word word)
+    //{
+    //    var values = PassReferenceValues(values.Item1, values.Item6, values.Item7);
+    //}
+}   
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,12 +1,38 @@
+using System.Runtime.InteropServices;
+
 public class Scripture
 {
     //declares member variables to store segmented scripture into a list for 1 verse, or 2 verses
-    private List<string> _verseWordsList;
+    private List<string> _verseWordsList1;
+
+    private List<string> _verseWordsList2;
     
+    //passes a parameter to scripture constructor to perform dependent action. Just 1 verse will
+    //create a single list, while 2 verses will create 2 lists.
         private Scripture()
     {
-        
-        _verseWordsList = new List<string>();
+        _verseWordsList1 = new List<string>();
+        _verseWordsList2 = new List<string>();
+    }
 
+    public void ParseScripCreateList(bool _multiVersesExist, string _verseText1, string _verseText2)
+    {
+        if (_multiVersesExist == true)
+        {
+            //parses _verseText1 and _verseText2, and assigns them to respective lists.
+
+            string[] words1 = _verseText1.Split(' ');
+            _verseWordsList1.AddRange(words1);
+
+            string[] words2 = _verseText2.Split(' ');
+            _verseWordsList2.AddRange(words2);
+        }
+        else
+        {
+            //parses _verseText1 and assigns it to a list.
+
+            string[] words1 = _verseText1.Split(' ');
+            _verseWordsList1.AddRange(words1);
+        }
     }
 }
