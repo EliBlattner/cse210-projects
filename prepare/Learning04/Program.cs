0fecb009cd13f7c7cf53b51162c4ecf344b0e028
Eli Blattner <139493532+EliBlattner@users.noreply.github.com>
2023-11-25 13:35:30 -0700

    .

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,14 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        EnemyRaider enemyRadier1 = new EnemyRaider();
-        string name = enemyRadier1.GetName();
-        int health = enemyRadier1.GetHitpoints();
-        int damage = enemyRadier1.GetDamage();
-        int aggroRange = enemyRadier1.GetAggroRadius();
-        bool canFly = enemyRadier1.GetCanFlyBool();
-        Console.WriteLine($"Name: {name}\nTotal Health: {health}\nDamage: {damage}\n");
-        Console.WriteLine($"Name: {name}\nTotal Health: {health}\nDamage: {damage}\nAggro Radius: {aggroRange}\nFlying: {canFly}\n");
-
+        
     }
 }
