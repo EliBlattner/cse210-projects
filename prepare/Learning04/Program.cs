b03b4bf6f6706f8144302996a4b684f2ca84ebe2
EliBlattner <139493532+EliBlattner@users.noreply.github.com>
2023-11-25 12:40:32 -0700

    playing around with inheritance

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        EnemyRaider enemyRadier1 = new EnemyRaider();
+        string name = enemyRadier1.GetName();
+        int health = enemyRadier1.GetHitpoints();
+        int damage = enemyRadier1.GetDamage();
+        int aggroRange = enemyRadier1.GetAggroRadius();
+        bool canFly = enemyRadier1.GetCanFlyBool();
+        Console.WriteLine($"Name: {name}\nTotal Health: {health}\nDamage: {damage}\n");
+        Console.WriteLine($"Name: {name}\nTotal Health: {health}\nDamage: {damage}\nAggro Radius: {aggroRange}\nFlying: {canFly}\n");
+
     }
 }
