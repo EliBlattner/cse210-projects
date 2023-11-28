8b3bf1f432664b68568160f9a96360aebb104352
Eli Blattner <139493532+EliBlattner@users.noreply.github.com>
2023-11-28 12:20:44 -0700

    fleshed out the activity methods and made progress on writing the program class.

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -1,6 +1,8 @@
 
+using System.Diagnostics.Contracts;
 using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
 
 public class Activity
 {
@@ -12,17 +14,20 @@ public class Activity
     //SHARED SUB CLASS COMMONALITIES HERE (PARENT CLASS)
 
     //Wheel that spins for 5 seconds, to be inserted as needed in other code.
-    public string SpinningWheel()
+    public void SpinningWheel()
     {
-        
-        
-        return 
+        foreach (string animation in _wheelCharacters)
+        {
+            Console.WriteLine(animation);
+            Thread.Sleep(500);
+            Console.Write("\b \b");
+        }
     }
     
 
     //Counts down from 5 to 1 at default, and takes parameter when user needs a 
     //higher countdown timer.
-    public void DescendingCountdownWait(int _desiredDuration)
+    public void DescendingCountdownWait()
     {
         for (int i = _desiredDuration ; i > 0; i--)
         {
@@ -33,7 +38,8 @@ public class Activity
         Console.WriteLine("Done");
     }
 
-    public void StartupMessage(string _activityType)
+    //Displays a startup message based on several instance fields
+    public void StartupMessage()
     {
         string introMessage = "null";
 
@@ -41,15 +47,28 @@ public class Activity
         {
             introMessage = "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.";
         }
+        else if (_activityType == "Reflecting")
+        {
+            introMessage = "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.";
+        }
+        else if (_activityType == "Listing")
+        {
+            introMessage = "This activty will help you reflect on the good things in your life by having you list as many things as you can in a certain area.";
+        }
         
-        
-        Console.WriteLine($"Welcome to the {_activityType} activity.")
+        Console.WriteLine($"Welcome to the {_activityType} activity. \n\n{introMessage}\n\nHow long in seconds, would you like for your session?:");
+        string userChoiceDurationString = Console.ReadLine();
+        int userChoiceDuration = int.Parse(userChoiceDurationString);
+
+        _desiredDuration = userChoiceDuration;
 
     }
 
 
-    public string EndMessage(_activityType)
+    public void EndMessage()
     {
-        return
+
+        Console.WriteLine($"\n\nWell Done!\n\nYou have completed another {_desiredDuration} seconds of the {_activityType} Activity.");
+
     }
 }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,10 +1,16 @@
 using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
+    static string _activityType;
+
     static void Main(string[] args)
     {
-
+        //Following section takes care of the menu interface and activity type
+        //assignment based on user input. Instantiates classes and executes methods and constructors.
         string _userMenuChoice = "99";
 
         Console.WriteLine("Welcome to the Wellness Program!\n1. Breathing Activity\n2. Reflecting Activity\n3. Listing Activity\n4. Quit");
@@ -13,6 +19,44 @@ class Program
         {
             Console.WriteLine("Enter Menu Choice. Type '4' to exit the program: ");
             _userMenuChoice = Console.ReadLine();
+
+            Activity activity1 = new Activity();
+
+            if (_userMenuChoice == "1")
+            {
+                _activityType = "Breathing";
+                activity1.StartupMessage();
+                BreathingActivity breathingActivity1 = new BreathingActivity();
+
+
+
+                activity1.EndMessage();
+            }
+            else if (_userMenuChoice == "2")
+            {
+                _activityType = "Reflecting";
+                activity1.StartupMessage();
+                ReflectingActivity reflectingActivity1 = new ReflectingActivity();=
+
+
+
+                activity1.EndMessage();
+            }
+            else if (_userMenuChoice == "3")
+            {
+                _activityType = "Listing";
+                activity1.StartupMessage();
+                ListingActivity listingActivity1 = new ListingActivity();
+
+
+
+                activity1.EndMessage();
+            }            
         }
     }
+    static string PassActivtyType()
+        {
+            return _activityType;
+        }
+
 }
