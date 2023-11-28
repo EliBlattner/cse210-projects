a59d26645a2252fea8a03b15a8eaab9d55eaf852
Eli Blattner <139493532+EliBlattner@users.noreply.github.com>
2023-11-27 18:28:57 -0700

    began writing the shared parent methods.

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -1,5 +1,32 @@
 
+using System.Runtime.CompilerServices;
+
 public class Activity
 {
+    protected string _activityType;
+
+    List<string> _wheelCharacters = new List<string> { "-", "'\'", "|", "/" };
+
     //SHARED SUB CLASS COMMONALITIES HERE (PARENT CLASS)
+
+    //Wheel that spins for 5 seconds, to be inserted as needed in other code.
+    public string SpinningWheel()
+    {
+        
+        
+        return 
+    }
+    
+
+    //Counts down from 5 to 1
+    public string DescendingCountdownWait(_desiredDuration)
+    {
+        
+        return
+    }
+
+    public string EndMessage(_activityType)
+    {
+        return
+    }
 }
